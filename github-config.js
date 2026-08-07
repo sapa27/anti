4dce3762be92bf86f8a2e6746c8c98acfb765ddb
sapa27 <natthapong.sripa@gmail.com>
2026-08-07 16:59:45 +0700

    Update github-config.js

--- github-config.js
+++ github-config.js
@@ -1,4 +1,4 @@
-/* GitHub Pages frontend configuration. Edit GAS_WEB_APP_URL after deploying GAS as Web App. */
+
 window.APP_GITHUB_CONFIG = Object.freeze({
   GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxufAfgbBgxCa62UwR2fY57NCRGHgj9TqFUwrdtGaBQwW0RJY0BzRq2KZ_pEsglNK_x/exec",
   BRIDGE_MODE: "github-bridge",
