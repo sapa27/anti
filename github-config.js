39980a1e0d663cce8127cc3689937327738ea89d
sapa27 <natthapong.sripa@gmail.com>
2026-08-08 11:07:58 +0700

    Update github-config.js

--- github-config.js
+++ github-config.js
@@ -1,6 +1,6 @@
 /* GitHub Pages frontend configuration. */
 window.APP_GITHUB_CONFIG = Object.freeze({
-  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyqWyFoYE5vhXgoi3vqDoSa4BbXulihVzkVJbhgaRJAH49Khy2iqn9Q8PBdFEWC_GVD/exec",
+  GAS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzxRmj8MAVTrayu56aghW0Lgd-OF8WB_VzWb90XqvwpRNswlmmzWEBHTgUq7x_yX2Xx/exec",
   BRIDGE_MODE: "github-bridge",
   BRIDGE_TIMEOUT_MS: 20000,
   REQUEST_TIMEOUT_MS: 90000,
